d2177e323db2eea4ee27e4c7f38fc624b4b5152b
NisathAndroid <nisathatadvantech@gmail.com>
2024-12-16 12:17:17 +0530

    my road map flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:LoginScreen(),
+    title: ,);
+  }
+}
